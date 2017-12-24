f91a1baecb03dbc94f793c1854db04216cc62539
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2017-12-24 03:20:46 +0100

    adapt to new persistence approach

--- src/y-indexeddb.js
+++ src/y-indexeddb.js
@@ -1,5 +1,9 @@
 /* global indexedDB, location, BroadcastChannel */
 
+import AbstractPersistence from '../../yjs/src/Persistence.js'
+import BinaryDecoder from '../../yjs/src/Binary/Decoder.js'
+import { integrateRemoteStructs } from '../../yjs/src/MessageHandler/integrateRemoteStructs.js'
+
 /*
  * Request to Promise transformer
  */
@@ -22,10 +26,10 @@ function openDB (room) {
   request.onupgradeneeded = function (event) {
     let db = event.target.result
     if (db.objectStoreNames.contains('model')) {
-      db.deleteObjectStore('messages')
+      db.deleteObjectStore('updates')
       db.deleteObjectStore('model')
     }
-    db.createObjectStore('messages', {autoIncrement: true})
+    db.createObjectStore('updates', {autoIncrement: true})
     db.createObjectStore('model')
   }
   return rtop(request)
@@ -33,120 +37,96 @@ function openDB (room) {
 
 const PREFERRED_TRIM_SIZE = 500
 
-export default function extendInexedDBPersistence (Y) {
-  class IndexedDBPersistence extends Y.AbstractPersistence {
-    constructor (y, opts) {
-      super(y, opts)
-      let room = this.y.options.connector.room
-      this.db = null
-      this.computedInitialContent = false
-      this.bufferedMessagesBeforeInitialContent = []
-      this._db = openDB(room)
-      this._db.then(db => {
-        this.db = db
-      })
-      this.channel = new BroadcastChannel('__yjs__' + room)
-      this.channel.addEventListener('message', e => {
-        let message = e.data
-        if (this.computedInitialContent) {
-          this.mutualExcluse(() => {
-            this.y.connector.receiveMessage('indexeddb', message, true)
-          })
-        } else {
-          this.bufferedMessagesBeforeInitialContent.push(message)
-        }
-      })
-      var token = true
-      this.mutualExcluse = function (f) {
-        if (token) {
-          token = false
-          try {
-            f()
-          } catch (e) {
-            token = true
-            throw new Error(e)
-          }
+export default class IndexedDBPersistence extends AbstractPersistence {
+  constructor (y, opts) {
+    super(opts)
+  }
+
+  init (y) {
+    let cnf = this.ys.get(y)
+    let room = y.room
+    cnf.db = null
+    cnf._db = openDB(room)
+    cnf._db.then(db => {
+      cnf.db = db
+    })
+    cnf.channel = new BroadcastChannel('__yjs__' + room)
+    cnf.channel.addEventListener('message', e => {
+      integrateRemoteStructs(y, new BinaryDecoder(e))
+    })
+    var token = true
+    cnf.mutualExcluse = function (f) {
+      if (token) {
+        token = false
+        try {
+          f()
+        } catch (e) {
           token = true
+          throw new Error(e)
         }
+        token = true
       }
-    }
-
-    destroy () {
-      this.db.close()
-    }
-
-    saveToMessageQueue (message) {
-      this.channel.postMessage(message)
-      let t = this.db.transaction(['messages'], 'readwrite')
-      let messagesStore = t.objectStore('messages')
-      messagesStore.put(message)
-      let cntP = rtop(messagesStore.count())
-      super.saveToMessageQueue(message)
-      cntP.then(cnt => {
-        if (cnt >= PREFERRED_TRIM_SIZE) {
-          this.persistDB()
-        }
-      })
-    }
-
-    saveOperations (message) {
-      this.mutualExcluse(() => {
-        super.saveOperations(message)
-      })
-    }
-
-    retrieveContent () {
-      return this._db.then(function (db) {
-        let t = db.transaction(['messages', 'model'], 'readonly')
-        let modelStore = t.objectStore('model')
-        let messagesStore = t.objectStore('messages')
-
-        return Promise.all([rtop(modelStore.get(0)), rtop(messagesStore.getAll())])
-      }).then(([model, messages]) => {
-        if (model != null) {
-          this.y.db.requestTransaction(function () {
-            this.fromBinary(model)
-          })
-        }
-        this.mutualExcluse(() => {
-          messages.forEach(m => {
-            this.y.connector.receiveMessage('indexeddb', m, true)
-          })
-        })
-        this.computedInitialContent = true
-        if (model != null || messages.length > 0) {
-          this.y.db.setUserId(Y.utils.generateUserId())
-        }
-      })
-    }
-
-    persistDB () {
-      if (!this.computedInitialContent) {
-        return Promise.reject(new Error('Unable to persistDB(). The content is not yet initialized via retrieveContent!'))
-      }
-      this.log('Room %s: Persisting Yjs model to Redis', this.y.options.connector.room)
-      let db = this.db
-
-      return new Promise((resolve, reject) => {
-        this.y.db.requestTransaction(function () {
-          let t = db.transaction(['messages', 'model'], 'readwrite')
-          let messagesStore = t.objectStore('messages')
-          let cntRequest = messagesStore.count()
-          cntRequest.onsuccess = () => {
-            if (cntRequest.result > 0) {
-              let modelStore = t.objectStore('model')
-              let buffer = this.toBinary()
-              modelStore.put(buffer, 0)
-              messagesStore.clear()
-            }
-            resolve()
-          }
-          cntRequest.onerror = function (e) {
-            reject(e.target.errorCode)
-          }
-        })
-      })
     }
   }
-  Y.extend('indexeddb', IndexedDBPersistence)
+
+  deinit (y) {
+    let cnf = this.ys.get(y)
+    cnf.db.close()
+    super.deinit(y)
+  }
+
+  saveUpdate (y, update) {
+    let cnf = this.ys.get(y)
+    cnf.channel.postMessage(update)
+    let t = cnf.db.transaction(['updates'], 'readwrite')
+    let updatesStore = t.objectStore('updates')
+    updatesStore.put(update)
+    let cntP = rtop(updatesStore.count())
+    cntP.then(cnt => {
+      if (cnt >= PREFERRED_TRIM_SIZE) {
+        this.persist(y)
+      }
+    })
+  }
+
+  saveStruct (y, struct) {
+    let cnf = this.ys.get(y)
+    cnf.mutualExcluse(() => {
+      super.saveStruct(y, struct)
+    })
+  }
+
+  retrieve (y) {
+    let cnf = this.ys.get(y)
+    return cnf._db.then(function (db) {
+      let t = db.transaction(['updates', 'model'], 'readonly')
+      let modelStore = t.objectStore('model')
+      let updatesStore = t.objectStore('updates')
+      return Promise.all([rtop(modelStore.get(0)), rtop(updatesStore.getAll())])
+    }).then(([model, updates]) => {
+      cnf.mutualExcluse(() => {
+        super.retrieve(y, model, updates)
+      })
+    })
+  }
+
+  persist (y) {
+    let cnf = this.ys.get(y)
+    let db = cnf.db
+    let t = db.transaction(['updates', 'model'], 'readwrite')
+    let updatesStore = t.objectStore('updates')
+    return rtop(updatesStore.getAll())
+    .then(function (updates) {
+      // apply pending updates before deleting them
+      AbstractPersistence.prototype.retrieve.call(this, y, null, updates)
+      // get binary model
+      let binaryModel = AbstractPersistence.prototype.persist.call(this, y)
+      // delete all pending updates
+      if (updates.length > 0) {
+        let modelStore = t.objectStore('model')
+        modelStore.put(binaryModel, 0)
+        updatesStore.clear()
+      }
+    })
+  }
 }
